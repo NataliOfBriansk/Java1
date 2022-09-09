57cbae7d8e347bd3e916215539c797fb90893287
NataliOfBriansk <prockopiewa.natalja@yandex.ru>
2022-09-10 02:04:23 +0700

    rename in Digit

--- ForsStaff/Average.java
+++ ForsStaff/Average.java
@@ -11,8 +11,8 @@ public class Average {
         System.out.print("Введите большее число: ");
         int max = scanner.nextInt();
 
-        int amount = 0;
-        int amountEven = 0;
+        int i = 0;
+        int j = 0;
         int sum = 0;
         int sumEven = 0;
         double average = 0;
@@ -20,17 +20,17 @@ public class Average {
 
         do {
             sum = sum + min;
-            ++amount;                         //количество чисел от min до max
+            ++i;                              //количество чисел всего от min до max
             if (min % 2 == 0) {
-                ++amountEven;                 //количество только четных чисел
+                ++j;                          //количество только четных чисел
                 sumEven = sumEven + min;
             }
             ++min;                            //переходит к следующему числу
         } while (!(min > max));
 
-        average = average + sum * 1.0 / amount;
+        average = average + sum * 1.0 / i;
 
-        averageEven = sumEven * 1.0 / amountEven;
+        averageEven = sumEven * 1.0 / j;
 
         System.out.println("Среднее арифметическое чисел в интервале от " + min + " до " + max + " составляет " + average);
         System.out.println("Среднее арифметическое четных чисел в интервале от " + min + " до " + max + " составляет " + averageEven);
